ebc03f65bda2e4cb7191ce87c577e3b12ed3195a
Néstor Schulten <nestorsito@outlook.com>
2021-06-22 22:37:41 +0200

    script movement snake

---
+++ Slither io Snake Crossover/Assets/Snake.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+
+    private Vector2 _direction = Vector2.right;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector2.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x, 
+            Mathf.Round(this.transform.position.y) + _direction.y, 
+            0.0f);
+
+    }
+
+}
