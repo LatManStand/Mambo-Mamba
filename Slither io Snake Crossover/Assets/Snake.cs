3b7ef4c1afb79a8ee2a3a2b4ea77e36c20fff6f4
Néstor Schulten <nestorsito@outlook.com>
2021-06-27 19:40:35 +0200

    wsdsadsdsd

--- Slither io Snake Crossover/Assets/Snake.cs
+++ Slither io Snake Crossover/Assets/Snake.cs
@@ -39,6 +39,12 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        
+        for(int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+        
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x, 
             Mathf.Round(this.transform.position.y) + _direction.y, 
